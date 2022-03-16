3eb205aefc7e20b7c9060c09d1c4bd845c1bed1f
Alexandre <fulanodetal@exemplo.br>
2022-03-16 18:01:27 -0300

    add

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static('./dist/BlogPessoal-Front-End'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/BlogPessoal-Front-End/'}),
 );
 
 // Start the app by listening on the default Heroku port
